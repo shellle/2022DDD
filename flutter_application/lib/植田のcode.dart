436bdd98511d23e153683e1810b72ec87e7b9a6c
Jinzaurusu <91837865+Jinzaurusu@users.noreply.github.com>
2022-11-20 23:39:01 +0900

    Update 植田のcode.dart

--- flutter_application/lib/植田のcode.dart
+++ flutter_application/lib/植田のcode.dart
@@ -6,7 +6,7 @@ class クラス名 {
 class Chat_group {
  String chat_group_name; //chat_group_nameがプロパティ　
  final chat_group_id;
- List chat_group_member_list;
+ List chat_group_member_list[];
  
  Chat_group({required this chat_group_name,required this.chat_group_id, required })
 
