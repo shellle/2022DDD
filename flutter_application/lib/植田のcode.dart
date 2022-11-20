8fd1fb7a20408db1cfd7091ec563e95d35ffba6f
Jinzaurusu <91837865+Jinzaurusu@users.noreply.github.com>
2022-11-20 22:59:28 +0900

    Create 植田のcode.dart

---
+++ flutter_application/lib/植田のcode.dart
@@ -0,0 +1,13 @@
+//chat_group
+class クラス名 {
+    型名 プロパティ名;
+
+
+ class Chat_group {
+ String chat_group_name; //chat_group_nameがプロパティ　
+ List chat_group_member_list;
+ int chat_group_id;
+
+ void //メソッド
+}
+
